10f07efce25828008115ec6b45ee6110b7e587fb
MJ Schanne <mj.schanne@capgemini.com>
2018-09-27 15:25:44 -0400

    assignment #1

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,80 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import UserOutput from './UserOutput/UserOutput';
+import UserInput from './UserInput/UserInput';
 import './App.css';
-import Person from './Person/Person';
 
 class App extends Component {
-  // only class based components contain state
-  // try to stay stateless wherever possible
-  // state is a non-optional name
-  // it helps the react component know when to re-render,
-  // based on changes to this property
   state = {
-    persons: [
-      { name: 'Max', age: 28 },
-      { name: 'Manu', age: 29 },
-      { name: 'Stephanie', age: 26 }
-    ],
-    otherState: 'some other value'
+    username: "username"
   }
 
-  // onclick becomes onClick, Handler suffix good
-  // to indicate it is not actively called
-  switchNameHandler = (newName) => {
-    // this.state.persons[0].name = 'Maximillian';
-
+  userInputHandler = (event) => {
     this.setState({
-      persons: [
-        { name: newName, age: 28 },
-        { name: 'Manu', age: 29 },
-        { name: 'Stephanie', age: 27 }
-      ]
-    })
-  }
-  // using paranthesis in jsx calls the funcion immediately in the DOM
-
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: 'Max', age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: 'Stephanie', age: 27 }
-      ]
-    })
+      username: event.target.value
+    });
   }
 
   render() {
-    // inline, so 'scoped' but not great...
-    const style = {
-      backgroundColor: 'white',
-      font: 'inherit',
-      border: '1px stolid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    }
-    // always upper case your component name 
-    // as lowercase is reserved for vanilla html
     return (
       <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
-        <button
-          style={style}
-          onClick={this.switchNameHandler.bind(this, 'Maximillian')}>Switch Name</button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age} />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'Max!!')}
-          changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age} />
+        <UserInput change={this.userInputHandler} value={this.state.username} />
+        <UserOutput output={this.state.username} />
+        <UserOutput output={this.state.username} />
+        <UserOutput output={this.state.username} />
       </div>
     );
-    // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
---
+++ react-complete-guide/src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const userOutput = (props) => {
+    return (
+        <div>
+            <p>lorem ipsum</p>
+            <p>{props.output}</p>
+        </div>
+    );
+}
+
+export default userOutput; 
